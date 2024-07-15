3354bc1dd9a791fa0c0081f89efdab7ff97e1211
tysondbv <140181582+tysondbv@users.noreply.github.com>
2024-07-15 18:39:48 +0530

    Update 8b.c

--- 8b.c
+++ 8b.c
@@ -1,25 +1,26 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
-int main(){
-int pid = fork();
-if(pid<0){
-printf("error");
-}
-else if(pid == 0){
-printf("child process execution started ids are  (PID: %d, PPID: %d)\n",getpid(),getppid());
-const char * fp = "file.txt";
-if(access(fp,F_OK)==0){
-printf("child process can access file1.txt");
-}
-if (access(fp, R_OK) == 0) {
- printf("Child Process: File %s is readable.\n", fp);}
-}
-else{
-wait(NULL);
-printf("Parent Process (PID: %d): Child process (PID: %d) exited.\n", getpid(), pid);
-}
-return 0;
+int main() {
+    int pid = fork();
+    if (pid < 0) {
+        printf("error\n");
+    }
+    else if (pid == 0) {
+        printf("Child process execution started. IDs are (PID: %d, PPID: %d)\n", getpid(), getppid());
+        const char *fp = "file.txt";
+        if (access(fp, F_OK) == 0) {
+            printf("Child process can access file.txt\n");
+        }
+        if (access(fp, R_OK) == 0) {
+            printf("Child Process: File %s is readable.\n", fp);
+        }
+    }
+    else {
+        wait(NULL);
+        printf("Parent Process (PID: %d): Child process (PID: %d) exited.\n", getpid(), pid);
+    }
+    return 0;
 }
