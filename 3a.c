93ea13a0bdffd4b05ead21f0c5e092b006e027fc
tysondbv <140181582+tysondbv@users.noreply.github.com>
2024-07-15 16:08:28 +0530

    Update 3a.c

--- 3a.c
+++ 3a.c
@@ -1,9 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+#include<dirent.h>
+#include<sys/stat.h>
 
-int main() {
-    execl("/bin/sh","sh","-c","ls -l",(char *)0);
+
+int main(){
+    DIR *dir;
+    struct stat fs;
+    struct dirent *entry;
+    
+    dir = opendir(".");
+    while((entry = readdir(dir))!=NULL){
+        stat(entry->d_name,&fs);
+        if(fs.st_size == 0){
+            printf("empty files %s :",entry -> d_name);
+            remove(entry -> d_name);
+        }
+    }
     return 0;
 }
